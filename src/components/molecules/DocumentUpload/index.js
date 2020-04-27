0a831cf84a8e8063d3648539e4052bf2eeaeff43
eshaamrabaney <eshaam.rabaney@gmail.com>
2020-04-27 17:11:28 +0200

    This does not seem to work

---
+++ src/components/molecules/DocumentUpload/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DocumentUpload = (props) => {
+
+  return (
+    <DocumentUpload>
+      <h1>Hello</h1>
+    </DocumentUpload>
+  );
+};
+
+export default DocumentUpload;
